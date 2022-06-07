eb45a2c2759bf485e792cda7ee5878e7735a2f21
Edwin Ortiz <edarcode@gmail.com>
2022-06-07 02:19:55 -0500

    init buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { favorite, home } from "./constants/paths";
+import Favorite from "./views/Favorite/Favorite";
+import Home from "./views/Home/Home";
+import NotFound from "./views/NotFound/NotFound";
+
 function App() {
-	return <div className="App">edarcode</div>;
+	return (
+		<Routes className="App">
+			<Route path={home} element={<Home />} />
+			<Route path="*" element={<NotFound />} />
+			<Route path={favorite} element={<Favorite />} />
+		</Routes>
+	);
 }
 
 export default App;
