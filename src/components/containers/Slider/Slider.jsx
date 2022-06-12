ea9a2b1edc2fac1205aa44ae2b656c68a54b8e2d
Edwin Ortiz <edarcode@gmail.com>
2022-06-12 07:31:15 -0500

    casi terminado el pt bendito carru T.T

--- src/components/containers/Slider/Slider.jsx
+++ src/components/containers/Slider/Slider.jsx
@@ -1,38 +1,68 @@
+import { useRef } from "react";
 import Card from "../../common/Card/Card";
 import css from "./style.module.css";
 
 export default function Slider({ data }) {
-	let isClick = false;
-	let cursorXspace = null;
+	const slider = useRef(null);
+	const sliderContainer = useRef(null);
+	let pressed = false;
+	let startX;
+	let x;
 
 	const handleOnMouseDown = e => {
-		isClick = true;
-		cursorXspace = e.nativeEvent.offsetX - e.target.offsetLeft;
+		pressed = true;
+		startX = e.nativeEvent.offsetX - sliderContainer.current.offsetLeft;
+		slider.current.style.cursor = "grabbing";
+	};
+	const handleOnMouseEnter = e => {
+		slider.current.style.cursor = "grab";
 	};
 	const handleOnMouseUp = e => {
-		isClick = false;
+		slider.current.style.cursor = "grab";
 	};
+	const handleOnMouseUpWindow = e => {
+		pressed = false;
+	};
+	window.addEventListener("mouseup", handleOnMouseUpWindow);
 
 	const handleOnMouseMove = e => {
-		if (!isClick) return;
+		if (!pressed) return;
 		e.preventDefault();
-		const offsetX = e.nativeEvent.offsetX;
-		const sliderContainer = document.getElementById("sliderContainer");
-		sliderContainer.style.left = `${offsetX - cursorXspace}px`;
+		x = e.nativeEvent.offsetX;
+		sliderContainer.current.style.left = `${x - startX}px`;
+		checkboundary(slider, sliderContainer);
 	};
 
 	return (
 		<div
+			ref={slider}
 			className={css.slider}
 			onMouseDown={handleOnMouseDown}
+			onMouseEnter={handleOnMouseEnter}
 			onMouseUp={handleOnMouseUp}
 			onMouseMove={handleOnMouseMove}
 		>
-			<section id="sliderContainer" className={css.slider__container}>
+			<section ref={sliderContainer} className={css.slider__container}>
 				{data.map(({ id, name, image }) => (
 					<Card key={id} title={name} img={image} />
 				))}
 			</section>
 		</div>
 	);
+}
+
+function checkboundary(slider, sliderCounter) {
+	const sliderRect = slider.current.getBoundingClientRect();
+	const sliderCounterRect = slider.current.getBoundingClientRect();
+
+	console.log(sliderCounterRect.right);
+	console.log(sliderRect.right);
+
+	if (parseInt(sliderCounter.current.style.left) > 0) {
+		sliderCounter.current.style.left = "0";
+	} else if (sliderCounterRect.right > sliderRect.right) {
+		sliderCounter.current.style.left = `-${
+			sliderCounterRect.width - sliderRect.width
+		}px`;
+	}
 }
