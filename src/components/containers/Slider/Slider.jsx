f4a4b313895332104e21e9204ae557d249c88870
Edwin Ortiz <edarcode@gmail.com>
2022-06-13 18:39:32 -0500

    monion

--- src/components/containers/Slider/Slider.jsx
+++ src/components/containers/Slider/Slider.jsx
@@ -1,62 +1,39 @@
-import { useRef } from "react";
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { useMediaScreen } from "../../../hooks/useMediaScreen";
+
 import Card from "../../common/Card/Card";
 import css from "./style.module.css";
 
 export default function Slider({ data }) {
-	const slider = useRef(null);
 	const sliderContainer = useRef(null);
-	let pressed = false;
-	let startX;
-	let x;
+	const { widthScreen } = useMediaScreen();
+	const [left, setLeft] = useState();
 
-	const handleOnMouseDown = e => {
-		pressed = true;
-		startX = e.nativeEvent.offsetX - sliderContainer.current.offsetLeft;
-		slider.current.style.cursor = "grabbing";
-	};
-
-	const handleOnMouseUp = e => {
-		slider.current.style.cursor = "grab";
-	};
-	const handleOnMouseUpWindow = e => {
-		pressed = false;
-	};
-	window.addEventListener("mouseup", handleOnMouseUpWindow);
-
-	const handleOnMouseMove = e => {
-		if (!pressed) return;
-		e.preventDefault();
-		x = e.nativeEvent.offsetX;
-		sliderContainer.current.style.left = `${x - startX}px`;
-		checkboundary(slider, sliderContainer);
-	};
+	useEffect(() => {
+		setLeft(
+			sliderContainer.current.scrollWidth - sliderContainer.current.offsetWidth
+		);
+	}, [widthScreen]);
 
 	return (
-		<div
-			ref={slider}
-			className={css.slider}
-			onMouseDown={handleOnMouseDown}
-			onMouseUp={handleOnMouseUp}
-			onMouseMove={handleOnMouseMove}
-		>
-			<section ref={sliderContainer} className={css.slider__container}>
+		<div className={css.slider}>
+			<motion.div
+				className={css.slider__container}
+				drag="x"
+				dragConstraints={
+					(sliderContainer.current && {
+						right: -10,
+						left: -left
+					}) ||
+					{}
+				}
+				ref={sliderContainer}
+			>
 				{data.map(({ id, name, image }) => (
 					<Card key={id} title={name} img={image} />
 				))}
-			</section>
+			</motion.div>
 		</div>
 	);
-}
-
-function checkboundary(slider, sliderCounter) {
-	const sliderRect = slider.current.getBoundingClientRect();
-	const sliderCounterRect = sliderCounter.current.getBoundingClientRect();
-
-	if (parseInt(sliderCounter.current.style.left) > 0) {
-		sliderCounter.current.style.left = "0";
-	} else if (sliderCounterRect.right < sliderRect.right) {
-		sliderCounter.current.style.left = `-${
-			sliderCounterRect.width - sliderRect.width
-		}px`;
-	}
 }
