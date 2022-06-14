f64b6ddb41bed67d64406af57a17fc3beea939bc
Edwin Ortiz <edarcode@gmail.com>
2022-06-14 12:31:41 -0500

    init search

--- src/components/common/Search/Search.jsx
+++ src/components/common/Search/Search.jsx
@@ -3,7 +3,7 @@ import css from "./style.module.css";
 export default function Search() {
 	return (
 		<label className={css.search}>
-			<input className={css.search__input} placeholder="Character's name" />
+			<input className={css.search__input} placeholder="Character's Name" />
 		</label>
 	);
 }
