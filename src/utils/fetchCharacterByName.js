3d5b828ea0966d6c1d65dd8e9a6f6b15e46fa27b
Edwin Ortiz <edarcode@gmail.com>
2022-06-15 04:06:16 -0500

    delete utils search by name

--- src/utils/fetchCharacterByName.js
+++
@@ -1,9 +0,0 @@
-import { apiRickAndMorty } from "../constants/urls";
-
-export const fetchCharacterByName = async ({ name }) => {
-	if (!name) return;
-	const character = await fetch(`${apiRickAndMorty}?name=${name}`, {
-		method: "GET"
-	});
-	return character.json();
-};
