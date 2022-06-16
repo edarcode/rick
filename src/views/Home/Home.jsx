b94c23fe3c4b54f787371630d8a11b8a2cd8cd27
Edwin Ortiz <edarcode@gmail.com>
2022-06-15 21:53:52 -0500

    loading para search characters

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Button from "../../components/common/Button/Button";
 import Search from "../../components/common/Search/Search";
+import Spinner from "../../components/common/Spinner/Spinner";
 import Characters from "../../components/containers/Characters/Characters";
 import Intro from "../../components/containers/Intro/Intro";
 import Paged from "../../components/containers/Paged/Paged";
@@ -14,18 +15,20 @@ export default function Home() {
 	const [characters, setCharacters] = useState();
 	const [page, setPage] = useState(1);
 	const [isSearching, setIsSearching] = useState(false);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
-		isSearching &&
+		if (isSearching) {
+			setLoading(true);
 			fetchCharacters({ name, page })
 				.then(data => setCharacters(data))
-				.then(() => {
-					const width = window.innerWidth;
-					if (width < 768) return window.scroll(0, 435);
-					if (width < 1280) return window.scroll(0, 320);
-					window.scroll(0, 500);
-				})
+				.then(() => setLoading(false))
 				.catch(err => console.log(err));
+			const width = window.innerWidth;
+			if (width < 768) return window.scroll(0, 435);
+			if (width < 1280) return window.scroll(0, 320);
+			window.scroll(0, 500);
+		}
 	}, [name, page, isSearching]);
 
 	const value = {
@@ -39,8 +42,9 @@ export default function Home() {
 
 	return (
 		<main className={css.main}>
+			{loading && <Spinner />}
 			<Intro />
-			{isSearching === false && (
+			{!isSearching && (
 				<Button
 					type="button"
 					content="Search characters"
@@ -50,7 +54,8 @@ export default function Home() {
 			<CharacterFiltersContext.Provider value={value}>
 				{isSearching && <Search />}
 			</CharacterFiltersContext.Provider>
-			{(characters && <Characters characters={characters} />) || <Sliders />}
+			{characters && !loading && <Characters characters={characters} />}
+			{!characters && <Sliders />}
 			{characters && (
 				<Paged
 					pageCount={characters.info.pages}
